ea24de8a85601666d4cc4132b5ef25697122ae7c
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-10-02 23:06:31 -0400

    setup profile card

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.scss'
+import UserProfileCard from './components/UserProfileCard/UserProfileCard'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <UserProfileCard />
+    </div>
+  )
+}
+
+export default App
+
